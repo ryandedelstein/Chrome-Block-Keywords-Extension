a0239321f488d244c41e50f79cc898795d04dd53
ryandedelstein <ryan.d.edelstein@vanderbilt.edu>
2020-12-09 10:45:49 -0500

    Pres 12/9

--- background.js
+++ background.js
@@ -1,19 +1,35 @@
-function bad_url(url) {
-    var bad_words = ["youtube", "facebook", "instagram", "bustabit"];
-    for(var i = 0; i < bad_words.length; i++) {
-        if(url.includes(bad_words[i])) {
-            return true;
-        }
-    }
-    return false;
-}
-
 chrome.webNavigation.onCompleted.addListener(function(details) {
+    //start function
     chrome.tabs.query({}, function (tabs) {
+
         for (var i = 0; i < tabs.length; i++) {
+
             if (bad_url(tabs[i].url)) {
+
                 chrome.tabs.remove(tabs[i].id);
+
             }
+
         }
     });
+    //end function
 });
+
+
+//returns true if contains one of the key words
+function bad_url(url) {
+
+    var bad_words = ["youtube", "facebook", "instagram", "bustabit"];
+
+    for(var i = 0; i < bad_words.length; i++) {
+
+        if(url.includes(bad_words[i])) {
+
+            return true;
+
+        }
+
+    }
+    
+    return false;
+}
